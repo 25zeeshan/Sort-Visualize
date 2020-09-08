6d24ae764f09116d64490f7e80152397affd5845
Ahsan <25zeeshanraza@gmail.com>
2020-09-09 02:32:50 +0530

    Updates

---
+++ precache-manifest.479840e7be91364ee68c9ce58d18823f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a952fc5f71e835ddc236c32baa9d4a42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48210ed3b315f225e634",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "83e5e946a3f8b7e808ab",
+    "url": "/static/css/main.65b99e60.chunk.css"
+  },
+  {
+    "revision": "48210ed3b315f225e634",
+    "url": "/static/js/2.a9f98c21.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a9f98c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e5e946a3f8b7e808ab",
+    "url": "/static/js/main.3449747a.chunk.js"
+  },
+  {
+    "revision": "fc6fa6f009dc3f59bcb1",
+    "url": "/static/js/runtime-main.efcc2786.js"
+  }
+]);
